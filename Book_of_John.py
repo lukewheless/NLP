53d61381f5cba6ca9bbeb0281b128855cfef19e1
Wheless <Luke_Wheless@baylor.edu>
2020-12-07 14:29:29 -0600

    enter

--- Book_of_John.py
+++ Book_of_John.py
@@ -11,27 +11,27 @@ from nltk.corpus import stopwords
 
 from operator import itemgetter 
 
-#blob = TextBlob(Path("book_of_John_text.txt").read_text())
-txt = open('book_of_John_text.txt', 'r')
+blob = TextBlob(Path("book_of_John_text.txt").read_text())
 
 stops = stopwords.words("english")  
 
-nouns = [w for (w, pos) in TextBlob(txt).pos_tags if pos[0] == 'NN']
+nouns = [w for w,pos in blob.tags if (pos == 'NN' or pos == 'NNP' or pos == 'NNS' or pos == 'NNPS') if w not in stops]
+nouns = TextBlob(str(nouns))
 
-'''
-for i in blob:
-    if blob[i].tags == "NN":
-        nouns = blob.word_counts.items() 
-'''
-
-items = [i for i in nouns if i[0] not in stops]
+items = nouns.word_counts.items() 
 
 sorted_items = sorted(items, key = itemgetter(1), reverse=True)
 
-top15 = sorted_items[:16]
+top15 = sorted_items[:15]
+top15 = str(top15)
 
-print(pd.DataFrame(top15, columns = ["word", "count"]))
+#print(pd.DataFrame(top15, columns = ["word", "count"]))
 
+mask = imageio.imread("mask_heart.png")
 
-#wc = WordCloud().generate(top15)
-#wc = wc.to_file("John.png")
+wc = WordCloud(colormap = "prism", mask = mask, background_color = "white")
+
+wc = wc.generate(top15)
+
+wc = wc.to_file("John.png")
+
